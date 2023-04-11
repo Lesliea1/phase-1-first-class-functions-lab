031f96575fc63d58619be5470c914d348e12fc70
leslie <alvarado.leslie12@yahoo.com>
2023-04-11 11:04:32 -0700

    lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (first) {
+return first.slice (0,2)
+}
+
+
+const returnLastTwoDrivers = function (last) {
+    return last.slice(2,4); 
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier (n) {
+    return function fare() {
+      return n * n;
+    }
+
+}
+const fareDoubler = function doubleFare (createFareMultiplier) {
+    return createFareMultiplier * 2; 
+}
+
+const fareTripler = function tripleFare (createFareMultiplier) {
+    return createFareMultiplier * 3;
+}
+
+function selectDifferentDrivers (drivers, returnFirstTwoDrivers) {
+    return returnFirstTwoDrivers (drivers);
+}
